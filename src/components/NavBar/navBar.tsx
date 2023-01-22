e8bbb7c6c04aef54d68cb35bc88b261f60a061bc
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-01-22 11:12:07 -0300

    Add responsive navbar

--- src/components/NavBar/navBar.tsx
+++ src/components/NavBar/navBar.tsx
@@ -4,47 +4,37 @@ import navBarStyles from "./navBar.module.css"
 function Navigation(props: { selected: string }) {
   return (
     <nav id="menu" className={navBarStyles.menu}>
-      <div>
-        <ul className={navBarStyles.menuUl}>
-          <li>
-            <Link
-              className={`${navBarStyles.link}`}
-              id={props.selected === "/" ? "active" : ""}
-              href="/"
-            >
-              gallery
-            </Link>
-          </li>
-          <li>
-            <Link
-              id={props.selected === "/bio" ? "active" : ""}
-              className={`${navBarStyles.link}`}
-              href="/bio"
-            >
-              bio
-            </Link>
-          </li>
-          <li>
-            <Link
-              style={{ pointerEvents: "none" }}
-              id={props.selected === "/shop" ? "active" : ""}
-              className={`${navBarStyles.link}`}
-              href="/shop"
-            >
-              shop
-            </Link>
-          </li>
-          <li>
-            <Link
-              id={props.selected === "/contact" ? "active" : ""}
-              className={`${navBarStyles.link}`}
-              href="/contact"
-            >
-              contact
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <ul className={navBarStyles.menuUl}>
+        <Link
+          className={`${navBarStyles.link}`}
+          id={props.selected === "/" ? "active" : ""}
+          href="/"
+        >
+          gallery
+        </Link>
+        <Link
+          id={props.selected === "/bio" ? "active" : ""}
+          className={`${navBarStyles.link}`}
+          href="/bio"
+        >
+          bio
+        </Link>
+        <Link
+          style={{ pointerEvents: "none" }}
+          id={props.selected === "/shop" ? "active" : ""}
+          className={`${navBarStyles.link}`}
+          href="/shop"
+        >
+          shop
+        </Link>
+        <Link
+          id={props.selected === "/contact" ? "active" : ""}
+          className={`${navBarStyles.link}`}
+          href="/contact"
+        >
+          contact
+        </Link>
+      </ul>
     </nav>
   )
 }
