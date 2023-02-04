bb655cb5071e5f8ec35d75eb253b5e76e005e579
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-02-03 21:30:10 -0300

    Add loading animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,20 @@
 import Print from "../components/Print/Print"
 import galleryStyle from "../styles/gallery.module.css"
 import { trpc } from "../utils/trpc"
+import Image from "next/image"
 
 function Gallery() {
   const { data } = trpc.print.list.useQuery()
   return (
     <>
       {!data && (
-        <h1
-          style={{
-            alignSelf: "center",
-            justifySelf: "center",
-            paddingTop: "10rem"
-          }}
-        >
-          Loading...
-        </h1>
+        <Image
+          style={{ alignSelf: "center", marginTop: "10rem" }}
+          height={300}
+          width={300}
+          alt="Loading"
+          src="/Loading.svg"
+        />
       )}
       <div className={galleryStyle.gallery}>
         {data?.map(({ name, url, toGallery }, index) => {
