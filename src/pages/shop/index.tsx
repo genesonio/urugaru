3609b2ccc493201582064dcd4580445f1156b114
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-02-04 16:44:02 -0300

    Add conditional rendering to page when have nothing to show

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,14 +1,34 @@
 import Link from "next/link"
-import type { Key } from "react"
-import Print from "../../components/Print/Print"
+import { Key } from "react"
+import Print from "../../components/Print"
 import shopStyle from "./shop.module.css"
 import { trpc } from "../../utils/trpc"
+import Image from "next/image"
 
 const Shop = () => {
   const { data } = trpc.print.list.useQuery()
+  const hasPrints = data?.some(({ toShop }) => toShop)
 
   return (
     <>
+      {!hasPrints && (
+        <div style={{ width: "90vw", textAlign: "center" }}>
+          <h1>Thank you for visiting our shop!</h1>
+          <h2>
+            We&apos;re still setting things up and not currently able to sell
+            any prints at the moment, but please check back soon for updates!
+          </h2>
+        </div>
+      )}
+      {!data && (
+        <Image
+          style={{ alignSelf: "center", marginTop: "10rem" }}
+          height={300}
+          width={300}
+          alt="Loading"
+          src="/Loading.svg"
+        />
+      )}
       <div className={shopStyle.shop}>
         {data?.map(
           ({ id, url, name, price, toShop }, index: Key | null | undefined) => {
