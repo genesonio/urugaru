e6b0275dae874cf0bbc7f869f97f54273839408f
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-02-05 00:48:01 -0300

    Move index.tsx (gallery page) to folder gallery, and create an index.tsx to make the / as gallery default

---
+++ src/pages/gallery/index.tsx
@@ -0,0 +1,38 @@
+import Print from "../../components/Print"
+import galleryStyle from "./gallery.module.css"
+import { trpc } from "../../utils/trpc"
+import Image from "next/image"
+
+function Gallery() {
+  const { data } = trpc.print.list.useQuery()
+  return (
+    <>
+      {!data && (
+        <Image
+          style={{ alignSelf: "center", marginTop: "10rem" }}
+          height={300}
+          width={300}
+          alt="Loading"
+          src="/Loading.svg"
+        />
+      )}
+      <div className={galleryStyle.gallery}>
+        {data?.map(({ name, url, toGallery }, index) => {
+          if (!toGallery) return
+          return (
+            <Print
+              name={name}
+              showPrice={!toGallery}
+              alt={name}
+              url={url}
+              key={index}
+            />
+          )
+        })}
+        {data === undefined || (data?.length === 0 && <p>Coming soon!</p>)}
+      </div>
+    </>
+  )
+}
+
+export default Gallery
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,7 @@
-import Print from "../components/Print"
-import galleryStyle from "../styles/gallery.module.css"
-import { trpc } from "../utils/trpc"
-import Image from "next/image"
+import Gallery from "./gallery"
 
-function Gallery() {
-  const { data } = trpc.print.list.useQuery()
-  return (
-    <>
-      {!data && (
-        <Image
-          style={{ alignSelf: "center", marginTop: "10rem" }}
-          height={300}
-          width={300}
-          alt="Loading"
-          src="/Loading.svg"
-        />
-      )}
-      <div className={galleryStyle.gallery}>
-        {data?.map(({ name, url, toGallery }, index) => {
-          if (!toGallery) return
-          return (
-            <Print
-              name={name}
-              showPrice={!toGallery}
-              alt={name}
-              url={url}
-              key={index}
-            />
-          )
-        })}
-        {data === undefined || (data?.length === 0 && <p>Coming soon!</p>)}
-      </div>
-    </>
-  )
+const Index = () => {
+  return <Gallery />
 }
 
-export default Gallery
+export default Index
