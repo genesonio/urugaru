7fc3c5786df5d1954e48b4ff8ecc7153665096ae
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-03-10 17:40:47 -0300

    feat: add buttons to add, minus and delete items, and responsivity

--- src/pages/shop/cart/index.tsx
+++ src/pages/shop/cart/index.tsx
@@ -25,6 +25,8 @@ const ShoppingCart = (): JSX.Element => {
     })
   })
 
+  console.log(cart.items)
+
   return (
     <section>
       {cart.items.length > 0 ? (
@@ -42,29 +44,59 @@ const ShoppingCart = (): JSX.Element => {
                       src={item.image}
                     />
                   </div>
-                  <div className="">
+                  <div>
                     <p className={style.name}>{item.name}</p>
+                    <div className={style.addIcon}>
+                      <Image
+                        onClick={() => cart.addOneToCart(item)}
+                        fill
+                        className={style.icon}
+                        alt="add icon"
+                        src="/add.svg"
+                      />
+                    </div>
                     <div className={style.info}>
                       <p className={style.text}>Qty: {item.quantity}</p>
-                      <p className={style.text}>$ {item.price.toFixed(2)}</p>
+                      <p className={style.text}>$ {item.price?.toFixed(2)}</p>
+                    </div>
+                    <div
+                      onClick={() => cart.removeOneFromCart(item.id)}
+                      className={style.minIcon}
+                    >
+                      <Image
+                        fill
+                        className={style.icon}
+                        alt="minus icon"
+                        src="/minus.svg"
+                      />
                     </div>
                   </div>
                 </div>
 
-                <div className="">
+                <div className={style.itemTotal}>
                   <p className={style.text}>Total:</p>
                   <p className={style.text}>
                     $ {(item.quantity * item.price).toFixed(2)}
                   </p>
+                  <div
+                    onClick={() => cart.deleteFromCart(item.id)}
+                    className={style.trashIcon}
+                  >
+                    <Image
+                      fill
+                      className={style.icon}
+                      alt="trash icon"
+                      src="/trash.svg"
+                    />
+                  </div>
                 </div>
               </div>
             )
           })}
           <div className={style.total}>
-            <p>Total items:</p>
-            <p>$ {total}</p>
+            <p>Total items: $ {total}</p>
+            <CheckoutButton lineItems={lineItems} />
           </div>
-          <CheckoutButton lineItems={lineItems} />
         </>
       ) : (
         <h1 className={style.empty}>
