f45f887d08843a8037ea75cc7561771c2f9f9292
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-03-26 20:46:26 -0300

    feat: add order to print table and sort when list

--- src/server/trpc/router/printResource.ts
+++ src/server/trpc/router/printResource.ts
@@ -2,9 +2,9 @@ import { z } from "zod"
 
 import { router, publicProcedure } from "../trpc"
 
-export const printResource = router({
+export const printResource = router( {
   upload: publicProcedure
-    .input(z.object({
+    .input( z.object( {
       name: z.string(),
       url: z.string(),
       price: z.number().optional(),
@@ -13,10 +13,10 @@ export const printResource = router({
       toShop: z.boolean(),
       toGallery: z.boolean()
 
-    }))
-    .mutation(async ({ ctx, input }) => {
+    } ) )
+    .mutation( async ( { ctx, input } ) => {
       const { name, url, price, description, dimension, toShop, toGallery } = input
-      await ctx.prisma.print.create({
+      await ctx.prisma.print.create( {
         data: {
           name,
           url,
@@ -26,17 +26,23 @@ export const printResource = router({
           toShop,
           toGallery
         }
-      })
-    }),
-  list: publicProcedure.query(async ({ ctx }) => {
-    const prints = await ctx.prisma.print.findMany()
+      } )
+    } ),
+  list: publicProcedure.query( async ( { ctx } ) => {
+    const prints = await ctx.prisma.print.findMany( {
+      orderBy: [
+        {
+          order: 'desc'
+        }
+      ]
+    } )
     return prints
-  }),
-  getOne: publicProcedure.input(z.object({ id: z.string() })).query(async ({ ctx, input }) => {
-    const item = await ctx.prisma.print.findUnique({ where: { id: input.id } })
+  } ),
+  getOne: publicProcedure.input( z.object( { id: z.string() } ) ).query( async ( { ctx, input } ) => {
+    const item = await ctx.prisma.print.findUnique( { where: { id: input.id } } )
     return item
-  }),
-  update: publicProcedure.input(z.object({
+  } ),
+  update: publicProcedure.input( z.object( {
     id: z.string(),
     name: z.string(),
     price: z.number().optional(),
@@ -44,9 +50,9 @@ export const printResource = router({
     dimension: z.string(),
     toShop: z.boolean(),
     toGallery: z.boolean()
-  })).mutation(async ({ ctx, input }) => {
+  } ) ).mutation( async ( { ctx, input } ) => {
     const { id, name, price, description, dimension, toShop, toGallery } = input
-    await ctx.prisma.print.update({
+    await ctx.prisma.print.update( {
       data: {
         name,
         price,
@@ -58,9 +64,9 @@ export const printResource = router({
       where: {
         id
       }
-    })
-  }),
-  delete: publicProcedure.input(z.object({ id: z.string() })).mutation(async ({ ctx, input }) => {
-    await ctx.prisma.print.delete({ where: { id: input.id } })
-  })
-})
+    } )
+  } ),
+  delete: publicProcedure.input( z.object( { id: z.string() } ) ).mutation( async ( { ctx, input } ) => {
+    await ctx.prisma.print.delete( { where: { id: input.id } } )
+  } )
+} )
